545bc3cff0939907048f31429892da3dde119abb
corea2244 <59365194+corea2244@users.noreply.github.com>
2023-01-15 02:39:24 +0900

    [part2-ch4-3-앨범별로 사진 추가하기 - DropDownPicker] 최종코드

--- App.js
+++ App.js
@@ -8,20 +8,65 @@ import {
   Text,
   TouchableOpacity,
 } from "react-native";
-import { getStatusBarHeight } from "react-native-iphone-screen-helper";
+
+import MyDropDownPicker from "./src/MyDropDownPicker";
+import TextInputModal from "./src/TextInputModal";
 import { useGallery } from "./src/use-gallery";
 
 const width = Dimensions.get("screen").width;
 const columnSize = width / 3;
 
 export default function App() {
-  const { images, imageWithAddButton, pickImages, deleteImage } = useGallery();
+  const {
+    imagesWithAddButton,
+    pickImage,
+    deleteImage,
+    selectedAlbum,
+    modalVisible,
+    openModal,
+    closeModal,
+    albumTitle,
+    setAlbumTitle,
+    addAlbum,
+    resetAlbumTitle,
+    isDropdownOpen,
+    openDropDown,
+    closeDropDown,
+    albums,
+    selectAlbum,
+  } = useGallery();
 
   const onPressOpenGallery = () => {
-    pickImages();
+    pickImage();
   };
+  const onLongPressImage = (iamgeId) => deleteImage(iamgeId);
+  const onPressAddAlbum = () => {
+    openModal();
+  };
+  const onSubmitEditing = () => {
+    if (!albumTitle) return;
 
-  const onLongPressImage = (imageId) => deleteImage(imageId);
+    // 1. 앨범에 타이틀 추가
+    addAlbum();
+
+    // 2. 모달 닫기 & TextInput의 value 초기화
+    closeModal();
+    resetAlbumTitle();
+  };
+  const onPressBackdrop = () => {
+    closeModal();
+  };
+  const onPressHeader = () => {
+    if (isDropdownOpen) {
+      closeDropDown();
+    } else {
+      openDropDown();
+    }
+  };
+  const onPressAlbum = (album) => {
+    selectAlbum(album);
+    closeDropDown();
+  };
 
   const renderItem = ({ item: { id, uri }, index }) => {
     if (id === -1) {
@@ -52,10 +97,31 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
+      {/* 앨범 DropDown, 앨범 추가 버튼 */}
+      <MyDropDownPicker
+        isDropdownOpen={isDropdownOpen}
+        onPressHeader={onPressHeader}
+        selectedAlbum={selectedAlbum}
+        onPressAddAlbum={onPressAddAlbum}
+        albums={albums}
+        onPressAlbum={onPressAlbum}
+      />
+
+      {/* 앨범을 추가하는 TextInputModal */}
+      <TextInputModal
+        modalVisible={modalVisible}
+        albumTitle={albumTitle}
+        setAlbumTitle={setAlbumTitle}
+        onSubmitEditing={onSubmitEditing}
+        onPressBackdrop={onPressBackdrop}
+      />
+
+      {/* 이미지 리스트 */}
       <FlatList
-        data={imageWithAddButton}
+        data={imagesWithAddButton}
         renderItem={renderItem}
         numColumns={3}
+        style={{ zIndex: -1 }}
       />
     </SafeAreaView>
   );
@@ -65,6 +131,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? getStatusBarHeight() : 0,
+    marginTop: Platform.OS === "android" ? 30 : 0,
   },
 });
--- src/use-gallery.js
+++ src/use-gallery.js
@@ -2,10 +2,20 @@ import { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 import { Alert } from "react-native";
 
+const defaultAlbum = {
+  id: 1,
+  title: "기본",
+};
+
 export const useGallery = () => {
   const [images, setImages] = useState([]);
+  const [selectedAlbum, setSelectedAlbum] = useState(defaultAlbum);
+  const [albums, setAlbums] = useState([defaultAlbum]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [albumTitle, setAlbumTitle] = useState("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const pickImages = async () => {
+  const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -21,6 +31,7 @@ export const useGallery = () => {
       const newImage = {
         id: lastId + 1,
         uri: result.assets[0].uri,
+        albumId: selectedAlbum.id,
       };
       setImages([...images, newImage]);
     }
@@ -42,18 +53,61 @@ export const useGallery = () => {
     ]);
   };
 
-  const imageWithAddButton = [
-    ...images,
+  const openModal = () => setModalVisible(true);
+  const closeModal = () => setModalVisible(false);
+  const openDropDown = () => setIsDropdownOpen(true);
+  const closeDropDown = () => setIsDropdownOpen(false);
+
+  const addAlbum = () => {
+    const lastId = albums.length === 0 ? 0 : albums[albums.length - 1].id;
+    const newAlbum = {
+      id: lastId + 1,
+      title: albumTitle,
+    };
+    setAlbums([...albums, newAlbum]);
+  };
+  const selectAlbum = (album) => {
+    setSelectedAlbum(album);
+  };
+
+  const resetAlbumTitle = () => setAlbumTitle("");
+
+  const filteredImages = images.filter(
+    (image) => image.albumId === selectedAlbum.id
+  );
+
+  const imagesWithAddButton = [
+    ...filteredImages,
     {
       id: -1,
       uri: "",
     },
   ];
 
+  useEffect(() => {
+    console.log("1) images", images);
+  }, [images]);
+
+  useEffect(() => {
+    console.log("2) filteredImages", filteredImages);
+  }, [filteredImages]);
+
   return {
-    images,
-    imageWithAddButton,
-    pickImages,
+    imagesWithAddButton,
+    pickImage,
     deleteImage,
+    selectedAlbum,
+    modalVisible,
+    openModal,
+    closeModal,
+    albumTitle,
+    setAlbumTitle,
+    addAlbum,
+    resetAlbumTitle,
+    isDropdownOpen,
+    openDropDown,
+    closeDropDown,
+    albums,
+    selectAlbum,
   };
 };
